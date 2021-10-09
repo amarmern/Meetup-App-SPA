3705cd0f917743188cb2e57abec8206c3ab30bac
Amrendra Kumar <amarmca51@gmail.com>
2021-10-09 17:26:31 +0530

    Added Firebase API and UseEffect Feature

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,58 @@
+import { useRef } from "react";
+import Card from "../ui/Card";
+import classes from "./NewMeetupForm.module.css";
+function NewMeetupForm(props) {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+  function submitHandler(event) {
+    event.preventDefault();
+    const title = titleInputRef.current.value;
+    const image = imageInputRef.current.value;
+    const address = addressInputRef.current.value;
+    const description = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title,
+      image,
+      address,
+      description
+    };
+
+    props.onAddMeetup(meetupData);
+  }
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" id="title" ref={titleInputRef} required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" id="image" ref={imageInputRef} required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" id="address" ref={addressInputRef} required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Description</label>
+          <textarea
+            type="text"
+            id="description"
+            ref={descriptionInputRef}
+            required
+            rows="5"
+          />
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup </button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default NewMeetupForm;
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,26 @@
+import { useHistory } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetup() {
-  return <div>New Meetups Page</div>;
+  const history = useHistory();
+  function addMeetupHandler(meetUpData) {
+    fetch(
+      "https://react-getting-started-7cb02-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetUpData),
+        headers: { "Content-Type": "application/json" }
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+  return (
+    <div>
+      New Meetups Page
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </div>
+  );
 }
 
 export default NewMeetup;
